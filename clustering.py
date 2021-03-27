9b4e3e8ab0fe7c164564bf5179e56950f43e0f09
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2021-03-27 14:39:07 -0300

    Grasp done

--- clustering.py
+++ clustering.py
@@ -1,212 +1,245 @@
-import math as m
+'''
+Modulo da classe do problema de clustering.
+'''
 import random as rand
-import time
 import numpy as np
 from scipy.spatial.distance import cdist, sqeuclidean
 
+def evaluate_state(min_dists):
+    '''
+        Return: SSE baseado na array de distancias
+        '''
+    return np.sum(min_dists)
+
+def get_diff_obs_state(current_label, k):
+    '''
+        Retorna uma label diferente da label atual entre as k disponiveis
+    '''
+    new_label = rand.randint(0, k-1)
+    while new_label == current_label:
+        new_label = rand.randint(0, k-1)
+    return new_label
+
+def escolhe_melhores(neighbourhood, num_best):
+    '''
+        Retorna os n melhores de uma vizinhança
+    '''
+    neighbourhood = np.sort(neighbourhood, order='sse')
+    return neighbourhood[:num_best]
 
 
 class Clustering:
-    def evaluate_state(self, min_dists):
-        return(np.sum(min_dists))
+    '''
+        Lida com a instãncia de um problema de clusterização.
+    '''
+    def __init__(self, data):
+        '''
+        valores do problema
+            data: ndarray size M x N
+                Cada linha da array é uma observação.
+                As colunas são os atributos de cada observação
+            num_obs: int
+                Número de observações no dataset
+            num_feat: int
+                numero de features (atributos) no dataset
+        '''
+        self.data = data
+        self.num_obs = len(data)
+        self.num_feat = len(data[0])
 
-    def generate_initial_centroids(self, data, k):
-        # Seleciona aleatoriamente K linhas para serem os centroides
-        points = np.random.choice(data.shape[0], size=k, replace=False)
-        return data[points]
+    def generate_initial_centroids(self, k):
+        '''
+            Seleciona aleatoriamente K linhas para serem os centroides
+        '''
+        points = np.random.choice(self.data.shape[0], size=k, replace=False)
+        return self.data[points].copy()
 
-    """
-        Update_cluster_means
-            Parameters:
-                data : M x N ndarray 
-                    observation matrix.
-                labels : int ndarray
-                    array of the labels of the observations.
-                k : int
-                    The number of centroids (codes).
-            Returns:
-                clusters: k x n ndarray
-                new centroids matrix
-                has_members : ndarray
-                    A boolean array indicating which clusters have members.
-    """
-    def update_cluster_means(self, data, labels, k):
-        num_obs = len(data)
-        num_feat = len(data[0])
-        clusters = np.zeros((k, num_feat), dtype=data.dtype)
+    def update_centroids(self, labels, k):
+        """
+        Parameters:
+            labels : int ndarray
+                array of the labels of the observations.
+            k : int
+                The number of centroids (codes).
+        Returns:
+            centroids: k x n ndarray
+            new centroids matrix
+            has_members : ndarray
+                A boolean array indicating which clusters have members.
+        """
+        centroids = np.zeros((k, self.num_feat), dtype=self.data.dtype)
 
         # sum of the numbers of obs in each cluster
         obs_count = np.zeros(k, np.int)
 
-        for i in range(num_obs):
-            label = labels[i] 
+        for i in range(self.num_obs):
+            label = labels[i]
             obs_count[label] += 1
-            clusters[label] += data[i]
+            centroids[label] += self.data[i]
 
         for i in range(k):
             cluster_size = obs_count[i]
 
             if cluster_size > 0:
                 # Calculate the centroid of each cluster
-                clusters[i] = clusters[i] / cluster_size
+                centroids[i] = centroids[i] / cluster_size
 
         # Return a boolean array indicating which clusters have members
-        return clusters, obs_count > 0
+        return centroids, obs_count > 0
 
-
-    '''
-        assign_clusters
+    def assign_clusters(self, centroids):
+        '''
         Parametros:
-            data: ndarray size M x N
-            Cada linha da array é uma observação.
-            As colunas são os atributos de cada observação
-
             centroids: ndarray size k x N
-            Cada linha é um centroide
+                Cada linha é um centroide
         Retornos:
             labels: ndarray size M
-            Uma array contendo o index do cluster atribuido a cada observacao
-            min_dist: ndarray size M
-            Array contendo a distancia da i-ésima observação até o centroide mais proximo
-    '''
-    def assign_clusters(self, data, centroids):
-        dists = cdist(data, centroids, 'sqeuclidean')
+                Uma array contendo o index do cluster atribuido a cada observacao
+            min_dists: ndarray size M
+                Array contendo a distancia da i-ésima observação até o centroide mais proximo
+        '''
+        dists = cdist(self.data, centroids, 'sqeuclidean')
         labels = dists.argmin(axis=1)
-        min_dist = dists[np.arange(len(labels)), labels]
-
-        return labels, min_dist
-
-    # # Descrição da instancia do problema
-    # def descricao(self):
-    #     pass #TODO
-
-    # # Retorna o SSE do estado atual
-    # def avaliar(self, estado):
-    #     pass
-
-    # # Diz se um estado é valido (necessário?) //TODO
-    # def valido(self, estado):
-    #     pass
-
-    # # retorna estado considerado nulo (Existe um estado nulo?) //TODO
-    # def estadoNulo(self):
-    #     pass
-
-    # # Retorna um estado aleatorio (k centroides aleatorios)
-    # def estadoAleatorio(self):
-    #     pass
-    
-    # retorna os n melhores estados (centroides) numa lista de estados
-    # def nMelhores(self, estados, n):
-    #     evaluated_states = list(map(lambda x: (self.avaliar(x), estados.index(x)), estados))
-
-    #     # evaluated_states.sort(reverse = True)
-    #     evaluated_states.sort()
-
-    #     n_melhores = evaluated_states[:n]
-
-    #     # Retorna os n melhores na estrutura (sse, estado)
-    #     return n_melhores
-
-    # # Retorna tupla (sse, estado) do melhor estado
-    # def melhorEstado(self, estados):
-    #     melhor_estado = nMelhores(estados, 1)
-    #     if melhor_estado:
-    #         return melhor_estado[0]
-    #     return []
-    
-    # # Executa a busca pelo metodo de busca passado
-    # def busca(self, estado, metodoBusca, tempo, **argumentos):
-    #     if argumentos:
-    #         metodoBusca(self, estado, tempo, **argumentos)
-    #     else:
-    #         metodoBusca(self, estado, tempo)
+        min_dists = dists[np.arange(len(labels)), labels]
+        return labels, min_dists
 
 
-    # # PROBLEMA: SIMULATED ANNEALING
-    # # função que aceita ou não um vizinho de um estado
-    # def aceitarVizinho(self, estadoAtual, vizinho, t):
-    #     if not self.valido(vizinho):
-    #         return False
-    #     elif self.melhorEstado([estadoAtual, vizinho]) == vizinho:
-    #         return True
-    #     else:
-    #         valor_vizinho = self.avaliar(vizinho)
-    #         valor_atual = self.avaliar(estadoAtual)
-    #         # simmulated annealing calc
-    #         p = 1/(m.exp((valor_atual - valor_vizinho)/t))
-    #         p = p if p >= 0 else -p
-    #         n = rand.random()
-    #         return n < p
 
-    # # PROBLEMA: ALGORITMO GENETICO
-    # # função de seleção de sobreviventes
-    # def selecao(self, estados):
-    #     pass
+    def __init_neighbourhood(self):
+        '''
+            Inicia uma vizinhança nula com N observacoes.
+            O index da observação é guardado para propositos de ordenação.
 
-    # # crossover de estados
-    # def crossover(self, estado1, estado2):
-    #     pass
+        Estrutura:
+                    (index, label, sse)
+        nbhood = [
+                    [  (0,   0,   0.0)],  #obs0
+                    [  (1,   0,   0.0)],  #obs1
+                    [  (2,   0,   0.0)]   #obs2
+                            ....       ]
+        '''
 
-    # # mutação num estado
-    # def mutacao(self, estado):
-    #     pass
+        index_col = np.arange(self.num_obs, dtype=np.int) #guarda o index mesmo se ordernar pelo sse
+        label_col = np.zeros(self.num_obs, dtype=np.int)  # novo label do i-ésimo ponto
+        sse_col = np.zeros(self.num_obs, dtype=np.float32)   # novo sse do i-ésimo estado
 
-    # # gera uma população a partir de um individuo
-    # def gerarPopulacao(self, populacao, tamanho):
-    #     pass
+        neighbourhood = np.zeros(self.num_obs, dtype={'names':('index', 'label', 'sse'),
+                                                'formats':('i4', 'i4', 'f4')})
+        neighbourhood['index'] = index_col
+        neighbourhood['label'] = label_col
+        neighbourhood['sse'] = sse_col
+        return neighbourhood
 
-    # # retorna o melhor de uma geração e seu sse (OU SEM SSE?)
-    # def melhorDaGeracao(self, estados):
-    #     melhor = self.melhorEstado(estados)
-    #     if melhor:
-    #         return melhor
-    #     return self.estadoNulo()
-
-    # PROBLEMA: GRASP
-
-    def __get_another_obs_state(self, current_label, k):
-        new_label = rand.randint(0, k-1)
-        while new_label == current_label:
-            new_label = rand.randint(0, k-1)
-        return new_label
-
-    def generate_neighborhood(self, data, centroids, labels, min_dists):
-        sse = self.evaluate_state(min_dists)
+    def generate_nbhood(self, centroids, labels, min_dists):
+        '''
+            gera uma vizinhança do estado atual.
+        '''
+        sse = evaluate_state(min_dists)
         k = len(centroids)
-        num_obs = len(data)
 
-        neighborhood = np.array([
-            np.arange(num_obs), # guardar o index mesmo se ordernar
-            np.zeros(num_obs),  # novo label do i-ésimo ponto
-            np.zeros(num_obs)   # novo sse do i-ésimo estado
-        ])
+        neighborhood = self.__init_neighbourhood()
 
-        for i in range(num_obs):
+        for i in range(self.num_obs):
             # Modifica o estado (label) do ponto i
-            new_label = self.__get_another_obs_state(labels[i], k)
-            neighborhood[1][i] = new_label
+            new_label = get_diff_obs_state(labels[i], k)
+            neighborhood[i]['label'] = new_label
 
             old_distance = min_dists[i]
-            new_distance = sqeuclidean(data[i], centroids[new_label])
+            new_distance = sqeuclidean(self.data[i], centroids[new_label])
 
             new_sse_for_state = sse - old_distance + new_distance
-            neighborhood[2][i] = new_sse_for_state
+            neighborhood[i]['sse'] = new_sse_for_state
 
         return neighborhood
 
 
-    def contrucao_gulosa(self, estado, m, tempo):
-        start = time.process_time()
+# # Descrição da instancia do problema
+# def descricao(self):
+#     pass #TODO
 
-        melhor = self.generate_initial_centroids(data, k)
+# # Diz se um estado é valido (necessário?) //TODO
+# def valido(self, estado):
+#     pass
+
+# # retorna estado considerado nulo (Existe um estado nulo?) //TODO
+# def estadoNulo(self):
+#     pass
+
+# # Retorna um estado aleatorio (k centroides aleatorios)
+# def estadoAleatorio(self):
+#     pass
+
+# retorna os n melhores estados (centroides) numa lista de estados
+# def nMelhores(self, estados, n):
+#     evaluated_states = list(map(lambda x: (self.avaliar(x), estados.index(x)), estados))
+
+#     # evaluated_states.sort(reverse = True)
+#     evaluated_states.sort()
+
+#     n_melhores = evaluated_states[:n]
+
+#     # Retorna os n melhores na estrutura (sse, estado)
+#     return n_melhores
+
+# # Retorna tupla (sse, estado) do melhor estado
+# def melhorEstado(self, estados):
+#     melhor_estado = nMelhores(estados, 1)
+#     if melhor_estado:
+#         return melhor_estado[0]
+#     return []
+
+# # Executa a busca pelo metodo de busca passado
+# def busca(self, estado, metodoBusca, tempo, **argumentos):
+#     if argumentos:
+#         metodoBusca(self, estado, tempo, **argumentos)
+#     else:
+#         metodoBusca(self, estado, tempo)
 
 
+# # PROBLEMA: SIMULATED ANNEALING
+# # função que aceita ou não um vizinho de um estado
+# def aceitarVizinho(self, estadoAtual, vizinho, t):
+#     if not self.valido(vizinho):
+#         return False
+#     elif self.melhorEstado([estadoAtual, vizinho]) == vizinho:
+#         return True
+#     else:
+#         valor_vizinho = self.avaliar(vizinho)
+#         valor_atual = self.avaliar(estadoAtual)
+#         # simmulated annealing calc
+#         p = 1/(m.exp((valor_atual - valor_vizinho)/t))
+#         p = p if p >= 0 else -p
+#         n = rand.random()
+#         return n < p
 
-    # #PROBLEMA: Branch and bound
-    # # função de estimativa
-    # def estimativa(self, estado, pessimista=True):
-    #     pass
+# # PROBLEMA: ALGORITMO GENETICO
+# # função de seleção de sobreviventes
+# def selecao(self, estados):
+#     pass
+
+# # crossover de estados
+# def crossover(self, estado1, estado2):
+#     pass
+
+# # mutação num estado
+# def mutacao(self, estado):
+#     pass
+
+# # gera uma população a partir de um individuo
+# def gerarPopulacao(self, populacao, tamanho):
+#     pass
+
+# # retorna o melhor de uma geração e seu sse (OU SEM SSE?)
+# def melhorDaGeracao(self, estados):
+#     melhor = self.melhorEstado(estados)
+#     if melhor:
+#         return melhor
+#     return self.estadoNulo()
+
+# #PROBLEMA: Branch and bound
+# # função de estimativa
+# def estimativa(self, estado, pessimista=True):
+#     pass
 
 
 
---
+++ grasp.py
@@ -0,0 +1,109 @@
+"""
+    Grasp metaheuristic for clustering problem
+"""
+import time
+import random as rand
+import numpy as np
+
+from clustering import Clustering, escolhe_melhores, evaluate_state
+
+def construcao_gulosa(problem: Clustering, state, num_best, max_time):
+    '''
+        construção gulosa do GRASP
+    '''
+    start = time.process_time()
+    end = 0.
+
+    k = len(state)
+
+    centroids = state.copy()
+
+    while end-start <= max_time:
+        aux_centroids = centroids.copy()
+        labels, min_dists = problem.assign_clusters(aux_centroids)
+
+        nbhood = problem.generate_nbhood(aux_centroids, labels, min_dists)
+        nbhood = escolhe_melhores(nbhood, num_best)
+
+        # Pegando index e label do estado vizinho aleatoriamente escolhido
+        obs_index, label, _ = nbhood[rand.randint(0,len(nbhood)-1)]
+
+        # Atribuindo novo estado
+        labels[obs_index] = label
+
+        # Calculando novo estado (centroides) da nova label
+        centroids, has_members = problem.update_centroids(labels, k)
+
+        # Caso algum centroide não possua membros, atribui a posicao anterior
+        if not has_members.all():
+            # Setting to previous centroid position
+            centroids[~has_members] = aux_centroids[~has_members]
+
+        end = time.process_time()
+
+    return centroids
+
+# local searh based on kmeans algorithn
+def local_search(problem: Clustering, state, max_time):
+    '''
+        Busca local baseada no k-means
+    '''
+    start = time.process_time()
+    end = 0.
+
+    k = len(state)
+    centroids = state.copy()
+
+    # main loop
+    # for i in range(iter):
+    while end-start <= max_time:
+        labels, _ = problem.assign_clusters(centroids)
+
+        # update centroids
+        new_centroids, has_members = problem.update_centroids(labels, k)
+
+        # check convergence
+        if np.array_equal(new_centroids, centroids):
+            break
+
+        if not has_members.all():
+            # Setting to previous position
+            new_centroids[~has_members] = centroids[~has_members]
+
+        centroids = new_centroids
+        end = time.process_time()
+
+    return centroids
+
+def grasp(problem: Clustering, k, num_best = 5, max_iter = 20, max_time = 1.):
+    '''
+        Grasp for clustering problems
+    '''
+    start = time.process_time()
+    end = 0
+    count = 0
+
+    opt_centroids = problem.generate_initial_centroids(k)
+    opt_labels, opt_min_dists = problem.assign_clusters(opt_centroids)
+    opt_sse = evaluate_state(opt_min_dists)
+
+    while (end-start) < max_time and count < max_iter:
+        # Construção
+        new_centroids = problem.generate_initial_centroids(k)
+        new_centroids = construcao_gulosa(problem, new_centroids, num_best, max_time/max_iter)
+        # Local search
+        new_centroids = local_search(problem, new_centroids, max_time/10)
+
+        new_labels, new_min_dists = problem.assign_clusters(new_centroids)
+        new_sse = evaluate_state(new_min_dists)
+
+        if new_sse < opt_sse:
+            opt_centroids = new_centroids
+            opt_labels = new_labels
+            opt_min_dists = new_min_dists
+            opt_sse = new_sse
+
+        end = time.process_time()
+        count += 1
+
+    return opt_centroids, count, opt_labels, opt_min_dists
