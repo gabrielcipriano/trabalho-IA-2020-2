525ec0fc65b705ffdc77ca30151fd7360d5d7e37
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2021-04-09 18:35:45 -0300

    zscores and times ploting

--- problema/ploting.py
+++ problema/ploting.py
@@ -4,85 +4,81 @@ import numpy as np
 import json
 
 import matplotlib.pyplot as plt
-import seaborn as sea
+import matplotlib.ticker as ticker
+import seaborn as sns
+
+def get_info_as_df(df, info, configs):
+    infos = []
+    for problem in df.keys():
+        infos.append(df[problem][info].to_numpy())
+    infos = pd.DataFrame(np.asarray(infos), 
+                        index = df.keys(), 
+                        columns= configs)
+    return infos
+
+# %%
+def build_results_dataframe(data):
+    results = data.copy()
+    # Renaming for label purposes
+    for d in list(results.keys()):
+        for p in list(results[d].keys()):
+            for k in list(results[d][p].keys()):
+                results[d][p+k] = pd.DataFrame(results[d][p][k])
+            results[d].pop(p)
+    return pd.DataFrame(results, columns = results.keys())
+
+# %%
+
+# %%
+def plot_metodo_results(metodo_df, configs, name, hparam_names):
+    df = metodo_df
+    configs = [tuple([round(i, 2) for i in l]) for l in configs]
+    # configs = [tuple(x) for x in configs]
+
+    #  Getting zscore df and tempos df
+    zscores = get_info_as_df(df, "zscore", configs)
+    tempos = get_info_as_df(df, "t", configs)
 
 
-def plot_metodo_results(metodo_data, name, hparam_names):
-    iris = metodo_data['iris']['results_by_k']
-    wine = metodo_data['wine']['results_by_k']
-    # Numpy parsing
-    for k, _ in iris.items():
-        for key, value in iris[k].items():
-            iris[k][key] = np.array(value)
-    for k, _ in wine.items():
-        for key, value in wine[k].items():
-            wine[k][key] = np.array(value)
-
-    # # Renaming for label purposes
-    # for k in list(iris.keys()):
-    #     iris["k"+str(k)+"_iris"] = iris.pop(k)
-    # for k in list(wine.keys()):
-    #     wine["k"+str(k)+"_wine"] = wine.pop(k)
-
-    df_iris = pd.DataFrame(iris)
-    df_wine = pd.DataFrame(wine)
-
-    print(df_iris["3"]["sse_mean"].shape)
-
-
-    # df_hparams = pd.DataFrame(metodo_data['iris']["hparams"], columns=hparam_names)
-    # df_hparams_iris = pd.concat([df_hparams, df_iris], axis=1)
-    # df_hparams['mean'] = df_iris.loc['sse_mean'].copy().mean(axis=1)
-    # df_hparams['desvio'] = df_iris.loc['sse_mean'].std(axis=0)
-    # df_hparams['rank'] = df_iris.loc['rank'].mean(axis=0)
-
-    # print(df_iris.iloc[:,0].shape)
-    # print(df_hparams.shape)
-
-    # print(df_hparams_iris)
-
-
-
-    '''
-        df_iris.iloc[ i, j] :
-        i = property index: [sse_mean, t_mean, z_score, rank]
-        j = k index: [3,...,22]
-    '''
-    # plot médias
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.set(xlabel='k', ylabel='Média', title ='Iris')
-    ax2.set(xlabel='k', ylabel='Média', title ='wine', ylim =(0, 5000000))
-    ax1.grid()
-    ax2.grid()
-    aux = df_iris.loc["sse_mean"]
-    # aux.columns = list(iris.keys())
-    sea.boxplot(data=aux, ax=ax1)
-    aux = df_wine.loc['sse_mean']
-    # # aux.columns = [2, 6, 9, 11, 33]
-    sea.boxplot(data=aux, ax=ax2)
-    # # plt.tight_layout()
-    plt.savefig(name+'_mean.png')
-
-    # boxplot tempos
-    _, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.set(xlabel='k', ylabel='Tempo', title ='Iris')
-    ax2.set(xlabel='k', ylabel='Tempo', title ='wine')
-    ax1.grid()
-    ax2.grid()
-    aux = df_iris.loc["t_mean"]
-    sea.boxplot(data=aux, ax=ax1)
-    aux = df_wine.loc["t_mean"]
-    sea.boxplot(data=aux, ax=ax2)
+    # boxplot zscores and tempos
+    figsize = (7.047, 5.022)
+    _, (ax1, ax2) = plt.subplots(ncols=2, sharey=True, figsize=figsize)
+    ax1.set(xlabel='z-score', ylabel='Hiperparâmetros: (' + ', '.join(hparam_names) + ")")
+    ax2.set(xlabel='Tempo')
+    sns.boxplot(data=zscores, ax=ax1, orient="h", palette="Set3")
+    sns.boxplot(data=tempos, ax=ax2,orient="h", palette="Set3")
+    plt.suptitle(name)
     plt.tight_layout()
-    plt.savefig(name+'_time.png')
-
-
-
+    plt.savefig(name+'_zscore_and_time_plot.png',dpi=200)
         
 # %%
-train_data = {}
-with open('../train_results.txt') as json_file:
-    train_data = json.load(json_file)
+def main():
+    train_data = {}
+    with open('../train_results_9april.json') as json_file:
+        train_data = json.load(json_file)
+    results = train_data["results"]
+    configs = train_data["hparams"]
+    info = {
+        "sa": {
+            "name": "Simulated Annealing",
+            "param_names": ('t','alfa','iter_max')
+        },
+        "grasp": {
+            "name": "GRASP",
+            "param_names": ('numIter', 'numBest')
+        },
+        "gen": {
+            "name": "Genetic Algorithm",
+            "param_names": ('tamPopulacao', 'tCross', 'tMut')
+        },
+    }
+
+    df = build_results_dataframe(results)
+
+    metodos = results.keys()
+    for m in metodos:
+        plot_metodo_results(df[m], configs[m], info[m]["name"], info[m]["param_names"])
 # %%
-plot_metodo_results(train_data['sa'], "Simulated Annealing", ['t','alfa','iter_max'])
+if __name__=="__main__":
+    main()
 # %%
